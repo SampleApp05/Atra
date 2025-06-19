34248b285e901aaebaca7b9d1e8629c0f0a7fb4e
Daniel Velikov <dvelikov@gameaccount.com>
2025-06-19 10:09:42 +0300

    [Atra] Add Shell project

---
+++ Atra/AtraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtraApp.swift
+//  Atra
+//
+//  Created by Daniel Velikov on 19.06.25.
+//
+
+import SwiftUI
+
+@main
+struct AtraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AtraTests/AtraTests.swift
@@ -0,0 +1,17 @@
+//
+//  AtraTests.swift
+//  AtraTests
+//
+//  Created by Daniel Velikov on 19.06.25.
+//
+
+import Testing
+@testable import Atra
+
+struct AtraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
